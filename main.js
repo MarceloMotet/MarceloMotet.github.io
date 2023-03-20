af91b028b4a6552542e321bfc512c2ecd6a7c939
MarceloMotet <108034386+MarceloMotet@users.noreply.github.com>
2023-03-20 19:56:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
-
-navToggle.addEventListener("click", () => {
-navMenu.classList.toggle("nav-menu_visible");
-
-if (navMenu.classList.contains("nav-menu_visible")) {
-  navToggle.setAttribute("aria-label", "Cerrar menú");
-} else {
-  navToggle.setAttribute("aria-label", "Abrir menú");
-}
-});
-
-
-
-
-  var formulario = document.getElementById("miFormulario");
+var formulario = document.getElementById("miFormulario");
 
 formulario.addEventListener("submit", function(event) {
   event.preventDefault(); // previene la recarga de la página
@@ -22,31 +6,36 @@ formulario.addEventListener("submit", function(event) {
   var nombres = document.getElementById("nombre").value;
   var apellidos = document.getElementById("apellido").value;
   var correo = document.getElementById("correo").value;
-  var contraseña = document.getElementById("contraseña").value;
+  var comentarios = document.getElementById("comentarios").value;
+ 
 
   if (nombres === "") {
     alert("Es obligatorio ingresar su nombre");
     document.getElementById("nombre").focus();
-  } else if (apellidos === "") {
+  } 
+    else if (apellidos === "") {
     alert("Es obligatorio ingresar su apellido");
     document.getElementById("apellido").focus();
   }
     else if (correo === "") {
-    alert("Es obligatorio ingresar su apellido");
+    alert("Es obligatorio ingresar su correo");
     document.getElementById("correo").focus();
-    }
-    else if (contraseña === "") {
-    alert("Es obligatorio ingresar su apellido");
-    document.getElementById("contraseña").focus();
-    
-
-  } else {
-    console.log(nombres + " " + apellidos+ ", " + correo + ", " + contraseña);
+  }
+    else if (comentarios === "") {
+    alert("Es obligatorio ingresar sus comentarios");
+    document.getElementById("comentarios").focus();        
+  }
+    else {
+    console.log(nombres + " " + apellidos + ", " + correo + ", " + comentarios);
     document.getElementById("nombre").value = "";
     document.getElementById("apellido").value = "";
     document.getElementById("correo").value = "";
-    document.getElementById("contraseña").value = "";
+    document.getElementById("comentarios").value = "";
     document.getElementById("nombre").focus();
   }
 });
 
+function saludar(nombre,apellido){
+  console.log("Hola "+ nombre+" "+apellido);
+  alert("Bienvenido "+ nombre+" "+apellido);
+}
